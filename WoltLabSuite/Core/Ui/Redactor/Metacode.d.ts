6f14c24186cc5a50cce509d7e3eef78b92169d0c
Tim Düsterhus <duesterhus@woltlab.com>
2023-02-23 11:29:53 +0100

    Remove the deleted redactor types

--- WoltLabSuite/Core/Ui/Redactor/Metacode.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Converts `<woltlab-metacode>` into the bbcode representation.
- *
- * @author  Alexander Ebert
- * @copyright  2001-2019 WoltLab GmbH
- * @license  GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @woltlabExcludeBundle tiny
- */
-export declare function convertFromHtml(editorId: string, html: string): string;
--- WoltLabSuite/Core/Ui/Redactor/Table.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @woltlabExcludeBundle tiny
- */
-type CallbackSubmit = () => void;
-interface TableOptions {
-    submitCallback: CallbackSubmit;
-}
-export declare function showDialog(options: TableOptions): void;
-export {};
