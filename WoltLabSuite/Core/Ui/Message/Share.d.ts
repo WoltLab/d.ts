5a78f087302ecbe1b673c98af7f2fc7d6de5e392
WoltLab GmbH <woltlab@woltlab.com>
2023-08-17 15:40:24 +0000

    Update WoltLab/WCF definitions
    Generated using:
    
        npx tsc --declaration --emitDeclarationOnly --outdir ./d.ts/
    
    from WoltLab/WCF@6314be43b2da85debb415ca4efcc7b8b8b7fad33

--- WoltLabSuite/Core/Ui/Message/Share.d.ts
+++ WoltLabSuite/Core/Ui/Message/Share.d.ts
@@ -4,5 +4,6 @@
  * @author  Marcel Werk
  * @copyright  2001-2019 WoltLab GmbH
  * @license  GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @deprecated 6.0 code is integrated in `Share/Dialog.ts`
  */
 export declare function init(): void;
