0227c12b160d6b2fd5ee09f72527c2283c581bab
Tim Düsterhus <duesterhus@woltlab.com>
2023-09-14 15:21:00 +0200

    Drop deleted WoltLab/WCF modules

--- WoltLabSuite/Core/Bbcode/Spoiler.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Generic handler for spoiler boxes.
- *
- * @author      Alexander Ebert
- * @copyright   2001-2020 WoltLab GmbH
- * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- */
-export declare function observe(): void;
