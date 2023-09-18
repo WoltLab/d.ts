bd6bbd6ed9e0263ce765ec533049b9293828988d
WoltLab GmbH <woltlab@woltlab.com>
2023-09-18 12:38:22 +0000

    Update WoltLab/com.woltlab.calendar.eventThread definitions
    Generated using:
    
        npx tsc --declaration --emitDeclarationOnly --outdir ./d.ts/
    
    from WoltLab/com.woltlab.calendar.eventThread@4776a67cba836c9b1d81939639da775c69b1184b

---
+++ WoltLabSuite/Calendar/Ui/Event/Thread/ShowParticipants.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Manages the event date box for threads.
+ *
+ * @author  Alexander Ebert
+ * @copyright 2001-2021 WoltLab GmbH
+ * @license WoltLab License <http://www.woltlab.com/license-agreement.html>
+ */
+export declare function setup(): void;
