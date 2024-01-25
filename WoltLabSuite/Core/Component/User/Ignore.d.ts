1c64bdbd502adf85ebd2b5c4fdd9d0323fd0f65a
WoltLab GmbH <woltlab@woltlab.com>
2024-01-25 15:40:36 +0000

    Update WoltLab/WCF definitions
    Generated using:
    
        npx tsc --declaration --emitDeclarationOnly --outdir ./d.ts/
    
    from WoltLab/WCF@5063b973f0aef1a85de780071d2b4966930fca79

---
+++ WoltLabSuite/Core/Component/User/Ignore.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Handles the user ignore buttons.
+ *
+ * @author Marcel Werk
+ * @copyright 2001-2024 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since 6.1
+ */
+export declare function setup(): void;
