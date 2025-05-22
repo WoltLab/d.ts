035eb84cca87b723d3f960a6b3495f2efd644865
WoltLab GmbH <woltlab@woltlab.com>
2025-05-22 15:44:15 +0000

    Update WoltLab/WCF definitions
    Generated using:
    
        npx tsc --declaration --emitDeclarationOnly --outdir ./d.ts/
    
    from WoltLab/WCF@505638098d8dc8b077006127f5a529c1bb09949e

---
+++ WoltLabSuite/Core/Form/Builder/FormGuard.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Prevents multiple submits of the same form by disabling the submit button.
+ *
+ * @author    Marcel Werk
+ * @copyright 2001-2025 WoltLab GmbH
+ * @license   GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since     6.2
+ */
+export declare function setup(form: HTMLFormElement): void;
