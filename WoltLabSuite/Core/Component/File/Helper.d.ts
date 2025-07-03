b15731b0226394567775cafe1a46ca3b36357cf2
WoltLab GmbH <woltlab@woltlab.com>
2025-07-03 15:43:42 +0000

    Update WoltLab/WCF definitions
    Generated using:
    
        npx tsc --declaration --emitDeclarationOnly --outdir ./d.ts/
    
    from WoltLab/WCF@0fc42f8d4c63308dda668707e622f12175ef8750

--- WoltLabSuite/Core/Component/File/Helper.d.ts
+++ WoltLabSuite/Core/Component/File/Helper.d.ts
@@ -2,5 +2,6 @@ import WoltlabCoreFileElement from "WoltLabSuite/Core/Component/File/woltlab-cor
 import "WoltLabSuite/Core/Component/File/woltlab-core-file";
 export declare function trackUploadProgress(element: HTMLElement, file: WoltlabCoreFileElement): void;
 export declare function removeUploadProgress(element: HTMLElement): void;
+export declare function getErrorMessageFromFile(file: WoltlabCoreFileElement): string;
 export declare function fileInitializationFailed(element: HTMLElement, file: WoltlabCoreFileElement, reason: unknown): void;
 export declare function insertFileInformation(container: HTMLElement, file: WoltlabCoreFileElement): void;
