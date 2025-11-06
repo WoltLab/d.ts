71f17557aeaf6013e052519b81562267a66ba49f
WoltLab GmbH <woltlab@woltlab.com>
2025-11-06 15:44:36 +0000

    Update WoltLab/WCF definitions
    Generated using:
    
        npx tsc --declaration --emitDeclarationOnly --outdir ./d.ts/
    
    from WoltLab/WCF@878069ea14d300b2b0b4989df8d96e16a7937a79

--- WoltLabSuite/Core/Form/Builder/Field/Controller/BadgeColor.d.ts
+++ WoltLabSuite/Core/Form/Builder/Field/Controller/BadgeColor.d.ts
@@ -1,11 +1,8 @@
 /**
- * @author    Olaf Braun
- * @copyright 2001-2024 WoltLab GmbH
+ * @author    Alexander Ebert
+ * @copyright 2001-2025 WoltLab GmbH
  * @license   GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @since     6.2
  * @woltlabExcludeBundle all
  */
-export declare class BadgeColorPreview {
-    #private;
-    constructor(fieldId: string, referenceFieldId: string, defaultLabelText: string);
-}
+export declare function setup(containerId: string, inputFieldId: string, placeholder: string): void;
