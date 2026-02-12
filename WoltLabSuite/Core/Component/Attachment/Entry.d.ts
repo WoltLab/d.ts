8684e02d0848644e455e14674039775c1f990201
WoltLab GmbH <woltlab@woltlab.com>
2026-02-12 15:58:13 +0000

    Update WoltLab/WCF definitions
    Generated using:
    
        npx tsc --declaration --emitDeclarationOnly --outdir ./d.ts/
    
    from WoltLab/WCF@f9b45ab4ddb25b1c0eefdccce742819b4518c4b1

--- WoltLabSuite/Core/Component/Attachment/Entry.d.ts
+++ WoltLabSuite/Core/Component/Attachment/Entry.d.ts
@@ -1,2 +1,6 @@
 import type WoltlabCoreFileElement from "../File/woltlab-core-file";
+export type FileProcessorData = {
+    attachmentID: number;
+    messageObjectID: number | null;
+};
 export declare function createAttachmentFromFile(file: WoltlabCoreFileElement, editor: HTMLElement): HTMLLIElement;
--- WoltLabSuite/Core/Component/Image/Viewer.d.ts
+++ WoltLabSuite/Core/Component/Image/Viewer.d.ts
@@ -2,9 +2,10 @@
  * @author Olaf Braun
  * @copyright 2001-2025 WoltLab GmbH
  * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since 6.2
  */
-import { CarouselSlide, FancyboxInstance } from "@fancyapps/ui";
+import { CarouselSlide } from "@fancyapps/ui";
 export declare function setup(): void;
 export declare function setupLegacy(): void;
-export declare function showFancybox(userSlides?: Array<CarouselSlide>): FancyboxInstance;
+export declare function showFancybox(userSlides?: Array<CarouselSlide>): void;
 export declare function getLocalization(): Record<string, string>;
