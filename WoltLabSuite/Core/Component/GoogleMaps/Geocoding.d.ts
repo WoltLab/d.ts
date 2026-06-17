773284f73e420b4a92308077ea9513e0d5e2f8e9
WoltLab GmbH <woltlab@woltlab.com>
2026-06-17 09:20:41 +0000

    Update WoltLab/WCF definitions
    Generated using:
    
        npx tsc --declaration --emitDeclarationOnly --outdir ./d.ts/
    
    from WoltLab/WCF@e97a545dd2a32bcb590aa002cdad1b4fe50e3aeb

--- WoltLabSuite/Core/Component/GoogleMaps/Geocoding.d.ts
+++ WoltLabSuite/Core/Component/GoogleMaps/Geocoding.d.ts
@@ -1,10 +1,3 @@
-/**
- * Provides geocoding functions for searching map locations.
- *
- * @author  Marcel Werk
- * @copyright  2001-2022 WoltLab GmbH
- * @license  GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- */
 import "./woltlab-core-google-maps";
 export type MoveMarkerEventPayload = {
     latitude: number;
