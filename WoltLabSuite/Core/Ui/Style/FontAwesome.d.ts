dba210389da8bcb1495635f3666d574af8255377
WoltLab GmbH <woltlab@woltlab.com>
2026-06-25 16:27:37 +0000

    Update WoltLab/WCF definitions
    Generated using:
    
        npx tsc --declaration --emitDeclarationOnly --outdir ./d.ts/
    
    from WoltLab/WCF@84946854fb42f4ec0a0609b31647020ee5fd8ce1

--- WoltLabSuite/Core/Ui/Style/FontAwesome.d.ts
+++ WoltLabSuite/Core/Ui/Style/FontAwesome.d.ts
@@ -6,10 +6,12 @@
  * @license  GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @woltlabExcludeBundle tiny
  */
-type CallbackSelect = (icon: string, forceSolid: boolean) => void;
+type CallbackSelect = (icon: string, forceSolid: boolean, value?: string, previewHtml?: string) => void;
 /**
  * Shows the FontAwesome selection dialog, supplied callback will be
- * invoked with the selection icon's name as the only argument.
+ * invoked with the selected icon's native data. Replacement
+ * implementations may pass an explicit stored value and preview html
+ * for non-native icons.
  */
 export declare function open(callback: CallbackSelect): void;
 export {};
