0d63cff2261e13efa1c888607b71e036a2377ed6
WoltLab GmbH <woltlab@woltlab.com>
2026-07-30 16:10:21 +0000

    Update WoltLab/WCF definitions
    Generated using:
    
        npx tsc --declaration --emitDeclarationOnly --outdir ./d.ts/
    
    from WoltLab/WCF@afdf3f09251cac9c9b8faa17888915ff01db2c1e

--- WoltLabSuite/Core/Component/GoogleMaps/MarkerLoader.d.ts
+++ WoltLabSuite/Core/Component/GoogleMaps/MarkerLoader.d.ts
@@ -8,5 +8,16 @@
 import WoltlabCoreGoogleMapsElement from "./woltlab-core-google-maps";
 import "./woltlab-core-google-maps";
 type AdditionalParameters = Record<string, unknown>;
+/**
+ * Loads the markers using the legacy `getMapMarkers` DBO action of the given class.
+ *
+ * @deprecated 6.3 use `setupWithEndpoint()` with a dedicated RPC endpoint instead
+ */
 export declare function setup(googleMaps: WoltlabCoreGoogleMapsElement, actionClassName: string, additionalParameters: AdditionalParameters): Promise<void>;
+/**
+ * Loads the markers using an RPC endpoint, e.g. `calendar/events/map-markers`.
+ *
+ * @since 6.3
+ */
+export declare function setupWithEndpoint(googleMaps: WoltlabCoreGoogleMapsElement, endpoint: string, additionalParameters?: AdditionalParameters): Promise<void>;
 export {};
